9b2b4c5a67c32bac80052a94fae4f204bb0f36cf
Yared <90713646+Yared04@users.noreply.github.com>
2022-08-14 20:53:45 +0300

    Time: 161 ms (43.37%), Space: 13.9 MB (35.50%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,43 +1,38 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         def checkRow(arr):
-            count = 0
             elements = set()
             for entry in arr:
+                if entry in elements: return False
                 if entry != ".":
-                    count += 1
                     elements.add(entry)
-            return count == len(elements)
+            return True
                     
         
         def checkCol(idx):
-            count = 0
             elements = set()
             for entry in board:
+                if entry[idx] in elements: return False
                 if entry[idx] != ".":
-                    count += 1
                     elements.add(entry[idx])
-            return count == len(elements)
+            return True
             
             
         def checkSubBox(startRow, endRow, startCol, endCol):
-            count = 0
             elements = set()
             for i in range(startRow, endRow):
                 for j in range(startCol, endCol):
+                    if board[i][j] in elements: return False
                     if board[i][j] != ".":
-                        count += 1
                         elements.add(board[i][j])
-            return count == len(elements)
+            return True
         
         for i in range(len(board)):
             row = checkRow(board[i])
-            if not row:
-                break
-        for i in range(len(board)):
             col = checkCol(i)
-            if not col:
+            if not row or not col:
                 break
+      
         subBox = checkSubBox(0,3,0,3) and checkSubBox(0,3,3,6) and checkSubBox(0,3,6,9) and checkSubBox(3,6,0,3) and checkSubBox(3,6,3,6) and checkSubBox(3,6,6,9) and checkSubBox(6,9,0,3) and checkSubBox(6,9,3,6) and checkSubBox(6,9,6,9)
         return row and col and subBox
         
