8ae8e927e2af65b8f6e7caa87cb359c004d3335c
Yared <90713646+Yared04@users.noreply.github.com>
2022-08-23 17:29:33 +0300

    Time: 103 ms (17.72%), Space: 16.1 MB (63.62%) - LeetHub

---
+++ 337-house-robber-iii/337-house-robber-iii.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        
+        lru_cache(maxsize = None)
+        def recur(root):
+            if not root:
+                return 0,0
+            
+            ## left
+            left = recur(root.left)
+            ## right
+            right = recur(root.right)
+            ## with
+            with_ = root.val + left[1] + right[1]
+            
+            ## with_out
+            without = 0
+            inc_left = left[0] + max(right[0], right[1])
+            inc_right = right[0] + max(left[0], left[1])
+            both = left[1] + right[1]
+            return with_, max(inc_left, inc_right, both)
+        
+        return max(recur(root))
