8e22746652114e0c0d9d74e56befecfcf5566ca9
Yared <90713646+Yared04@users.noreply.github.com>
2022-11-09 16:10:43 +0300

    Time: 191 ms (10.47%), Space: 16.1 MB (5.73%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        answer = set()
+        def helper(idx, new_string):
+            if idx == len(s):
+                answer.add(new_string)
+                return
+            ns = new_string
+            if 64 < ord(s[idx]) < 97:
+                ns = new_string[:idx] + chr(ord(new_string[idx]) + 32) + new_string[idx+1:]
+            elif ord(new_string[idx]) > 96:
+                ns = new_string[:idx] + chr(ord(s[idx]) - 32) + new_string[idx+1:]
+            
+            helper(idx + 1, ns)
+            helper(idx + 1, new_string)
+        
+        helper(0, s)
+        return list(answer)
+            
