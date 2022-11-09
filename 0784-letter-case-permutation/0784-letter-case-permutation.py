e85f4ac966cf97bb4b5ecfacd566bbef395b4c51
Yared <90713646+Yared04@users.noreply.github.com>
2022-11-09 16:11:15 +0300

    Time: 120 ms (54.33%), Space: 16.1 MB (5.73%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -5,13 +5,13 @@ class Solution:
             if idx == len(s):
                 answer.add(new_string)
                 return
-            ns = new_string
+            ns = None
             if 64 < ord(s[idx]) < 97:
                 ns = new_string[:idx] + chr(ord(new_string[idx]) + 32) + new_string[idx+1:]
             elif ord(new_string[idx]) > 96:
                 ns = new_string[:idx] + chr(ord(s[idx]) - 32) + new_string[idx+1:]
-            
-            helper(idx + 1, ns)
+            if ns:
+                helper(idx + 1, ns)
             helper(idx + 1, new_string)
         
         helper(0, s)
