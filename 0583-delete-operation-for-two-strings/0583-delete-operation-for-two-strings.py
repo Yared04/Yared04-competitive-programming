720085f8283b2f2a0fc6e7ca7532f38f509c7c08
Yared <90713646+Yared04@users.noreply.github.com>
2022-11-10 11:20:45 +0300

    Time: 290 ms (90.30%), Space: 17.7 MB (45.63%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -2,31 +2,16 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
-        memo = {}
-        def topDown(i, j):
-            if i == m and j == n:
-                return 0
-            if i == m:
-                return n - j
-            if j == n:
-                return m - i
-            
-            if (i,j) not in memo:
-                if word1[i] == word2[j]:
-                    memo[(i, j)] = topDown(i+1, j+1)
+        dp = [[0] * (m+1) for _ in range(n+1)]
+        
+        for i in range(n+1):
+            for j in range(m+1):
+                if i == 0 or j == 0:
+                    dp[i][j] = i + j
+                elif word2[i-1] == word1[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
                 else:
-                    memo[(i, j)] = 1 + min(topDown(i+1, j), topDown(i, j+1))
-            return memo[(i, j)]
-        return topDown(0,0)
-    
-        # n = len(word1)+1
-        # dp = [[0] * n for _ in range(len(word2)+1)]
-        # for i in range(len(word2)+1):
-        #     for j in range(len(word1)+1):
-        #         if i == 0 or j == 0:
-        #             dp[i][j] = i+j
-        #         elif word2[i-1] == word1[j-1]:
-        #             dp[i][j] = dp[i-1][j-1]
-        #         else:
-        #             dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1])
-        # return dp[-1][-1]
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1])
+        return dp[-1][-1]
+        
+      
