7ac2cb40d3f5ad1ef19035287d9f92dd4d12123b
Yared <90713646+Yared04@users.noreply.github.com>
2022-12-05 21:06:29 +0300

    Time: 5877 ms (6.78%), Space: 14.1 MB (27.28%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -7,14 +7,14 @@ class Solution:
                 return
             for i in range(1, n+1):
                 k = len(perm) + 1
-                if i not in visited and (i % k == 0 or k % i == 0):
-                    visited.add(i)
+                if not visited & 1<<i-1 and (i % k == 0 or k % i == 0):
+                    visited ^= 1<<i-1
                     perm.append(i)
                     backtrack(perm, visited)
-                    visited.remove(i)
+                    visited ^= 1<<i-1
                     perm.pop()
                     
-        backtrack([], set())
+        backtrack([], 0)
         return self.beautiful_arr
                     
         
