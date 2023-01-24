c696dc63df213530ccbb67cc8c90fe30bba8f911
Yared <90713646+Yared04@users.noreply.github.com>
2023-01-24 15:26:21 +0300

    Time: 139 ms (62.11%), Space: 13.8 MB (97.93%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,6 +1,8 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         self.count = 0
+        #taking each letter of s as a center and expanding left and right to check if the resulting substring
+        #is a palindrome as well
         def countPalindrome(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 self.count += 1
@@ -8,6 +10,8 @@ class Solution:
                 right += 1
 
         for i in range(len(s)):
+            #We should consider even and odd length substrings, meaning I should start from a single letter
+            #and from two consecutive letters as the center of a substring
             countPalindrome(i, i)
             countPalindrome(i, i+1)
         
