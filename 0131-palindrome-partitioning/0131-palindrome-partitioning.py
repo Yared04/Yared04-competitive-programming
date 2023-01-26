6d4bf014e20f0934a017337f143aa6acd1b6fea4
Yared <90713646+Yared04@users.noreply.github.com>
2023-01-26 23:58:45 +0300

    Time: 663 ms (84.21%), Space: 28.9 MB (87.76%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,13 +1,12 @@
 class Solution:
-    def isPalindrome(self, sequence):
-        left = 0
-        right = len(sequence) - 1
-        while left < right:
-            if sequence[left] != sequence[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
+    @lru_cache(None)
+    def isPalindrome(self, left, right, sequence):
+        if left >= right:
+            return True
+        if sequence[left] != sequence[right]:
+            return False
+        return self.isPalindrome(left+1, right-1, sequence)
+    
     def divide(self, idx, seq, comb, res):
         if idx == len(seq):
             res.append(comb[:])
@@ -15,7 +14,7 @@ class Solution:
         new_string = ""
         for i in range(idx, len(seq)):
             new_string += seq[i]
-            if self.isPalindrome(new_string):
+            if self.isPalindrome(0, len(new_string)-1, new_string):
                 comb.append(new_string)
                 self.divide(i + 1, seq, comb, res)
                 comb.pop()
