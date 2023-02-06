5cf067eeff7793f404f4b832d91eb733b274d919
Yared <90713646+Yared04@users.noreply.github.com>
2023-02-06 22:29:16 +0300

    Time: 140 ms (84.74%), Space: 18 MB (5.38%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        
+        @lru_cache(None)
+        def checkValidity(w):
+            count = 0
+            for i in range(len(w)):
+                pre = w[:i] + w[i+1:]
+                if pre in seen:
+                    count = max(count, checkValidity(pre))
+            return 1 + count
+
+        ans = 0
+        seen = set()
+        for word in words: 
+            seen.add(word)
+        for word in words:
+            ans = max(ans, checkValidity(word))
+        return ans
