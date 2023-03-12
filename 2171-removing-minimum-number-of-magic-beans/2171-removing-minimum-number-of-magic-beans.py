7ace0310eba40081950330e659cbe8d0ef25074c
Yared <90713646+Yared04@users.noreply.github.com>
2023-03-12 20:32:59 +0300

    Time: 1571 ms (27.70%), Space: 29.8 MB (18.92%) - LeetHub

--- 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.py
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.py
@@ -1,13 +1,18 @@
 class Solution:
     def minimumRemoval(self, beans: List[int]) -> int:
+        n = len(beans)
         beans.sort()
-        prefix_sum = [beans[0]]
-        for i in range(1,len(beans)):
-            prefix_sum.append(prefix_sum[-1] + beans[i])
-        print(prefix_sum)
-        ans = (prefix_sum[-1] - prefix_sum[0]) - beans[0] * (len(beans)-1)
-        print(ans)
-        for i in range(1, len(beans)):
-            ans = min(ans, (prefix_sum[-1] - prefix_sum[i]) - beans[i] * (len(beans)-i-1) + prefix_sum[i-1])
-        return ans
+        prefix = [*beans]
+        for i in range(1,n):
+            prefix[i] += prefix[i-1]
+        print(prefix)
+        res = (prefix[-1] - prefix[0]) - ((n-1) * beans[0])
+        print(res)
+        for i in range(1,n):
+            leftSum = prefix[i-1]
+            rightSum = prefix[-1] - prefix[i]
+            rightSum -= ( n-1-i) * beans[i]
 
+            res = min(res, rightSum + leftSum)
+
+        return res
