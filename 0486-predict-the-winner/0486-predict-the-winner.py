7f401beef55be06df86fabba25f6d7ac602330d9
Yared <90713646+Yared04@users.noreply.github.com>
2023-03-14 14:02:58 +0300

    Time: 106 ms (34.43%), Space: 13.9 MB (48.35%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,18 @@
 from functools import lru_cache
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        def findWinner(start, end):
-            if start == end:
-                return nums[start]
-            option1 = nums[start] - findWinner(start + 1, end)
-            option2 = nums[end] - findWinner(start, end - 1)
-            return max(option1, option2)
+        def findWinner(start, end, p1Score, p2Score, turn):
+            if start > end:
+                return p1Score >= p2Score
+            if turn:
+                return findWinner(start + 1, end, p1Score + nums[start], p2Score, False) or findWinner(start, end - 1, p1Score + nums[end], p2Score, False)
+            else:
+                return findWinner(start + 1, end, p1Score, nums[start] + p2Score, True) and findWinner(start, end - 1, p1Score, nums[end] + p2Score, True)
+                
             
             
             
-        return findWinner(0, len(nums)-1) >= 0
+        return findWinner(0, len(nums)-1, 0, 0, True)
             
             
             
