517d5821db771c3264ece76a02056c8a38931a64
Yared <90713646+Yared04@users.noreply.github.com>
2023-04-06 00:47:49 +0300

    Time: 5554 ms (5.36%), Space: 27.1 MB (7.14%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,22 +1,17 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         def check(cur_max, nums):
-            temp = 0
-            for i in range(len(nums)):
-                if nums[i] < cur_max:
-                    temp += cur_max - nums[i]
-                    
-                else: 
-                    if temp < nums[i] - cur_max:
-                        return False
-                    temp -= nums[i] - cur_max
-            return True
+            for i in range(len(nums)-1, 0, -1):
+                diff = nums[i] - cur_max
+                nums[i] -= max(diff, 0)
+                nums[i-1] += max(diff, 0)
+            return nums[0] <= cur_max
         
         low = 0
         high = max(nums)
         while low < high:
             mid = (low + high)//2
-            if check(mid, nums):
+            if check(mid, nums[:]):
                 high = mid 
             else:
                 low = mid + 1
